9ec11257567817b687c68006e9d4c3a244570d91
Robin Löffel <hi@robinloeffel.ch>
2024-07-05 13:48:32 +0200

    refactor!: cjs to esm, update dev deps, improve tests

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 /** @type {import("stylelint").Config} */
-module.exports = {
+export default {
 	extends: [
 		"stylelint-config-standard-scss",
 		"stylelint-config-recess-order",
@@ -12,40 +12,28 @@ module.exports = {
 	rules: {
 		"color-named": "never",
 		"property-no-unknown": null,
-		"declaration-property-value-disallowed-list": [
-			{
-				transition: "/all/"
-			},
-			{
-				message: "Avoid using \"all\" in transition declarations. Be more specific."
-			}
-		],
-		"selector-disallowed-list": [
-			"/:focus(?!-.*)/",
-			{
-				splitList: true,
-				message: "Avoid the \":focus\" pseudo-class. Use \":focus-visible\" instead."
-			}
-		],
+		"declaration-property-value-disallowed-list": [{
+			transition: "/all/"
+		}, {
+			message: "Avoid using \"all\" in transition declarations. Be more specific."
+		}],
+		"selector-disallowed-list": [ "/:focus(?!-.*)/", {
+			splitList: true,
+			message: "Avoid the \":focus\" pseudo-class. Use \":focus-visible\" instead."
+		}],
 		"@isnotdefined/no-obsolete": true,
-		"@stylistic/block-closing-brace-newline-after": [
-			"always",
-			{
-				ignoreAtRules: [ "if", "else" ]
-			}
-		],
+		"@stylistic/block-closing-brace-newline-after": [ "always", {
+			ignoreAtRules: [ "if", "else" ]
+		}],
 		"@stylistic/indentation": null,
 		"scss/at-mixin-argumentless-call-parentheses": "never",
 		"scss/at-use-no-unnamespaced": true,
 		"scss/at-use-no-redundant-alias": true,
 		"scss/block-no-redundant-nesting": true,
 		"scss/dimension-no-non-numeric-values": true,
-		"scss/dollar-variable-first-in-block": [
-			true,
-			{
-				ignore: [ "comments", "imports" ]
-			}
-		],
+		"scss/dollar-variable-first-in-block": [ true, {
+			ignore: [ "comments", "imports" ]
+		}],
 		"scss/dollar-variable-no-namespaced-assignment": true,
 		"scss/function-calculation-no-interpolation": true,
 		"scss/function-color-relative": true,
--- test/muy-bien.test.ts
+++ test/muy-bien.test.ts
@@ -1,5 +1,5 @@
-import { expect, test } from "vitest";
 import stylelint from "stylelint";
+import { expect, test } from "vitest";
 
 test("found no errors", async() => {
 	const { errored } = await stylelint.lint({ files: "./test/muy-bien.scss" });
